dc39bb03298359d4fe471414e17e76883a939403
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2011-10-24 23:56:55 +0000

    SVN commit 4

--- doc/PDFgen.sh
+++ doc/PDFgen.sh
@@ -3,6 +3,6 @@
 
 txt2html -b *.txt
 
-/home/sjplimp/tools/htmldoc/bin/htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_intro.html Section_start.html Section_commands.html Section_howto.html Section_example.html Section_perf.html Section_tools.html Section_modify.html Section_python.html Section_errors.html Section_history.html [a-z]*.html
+htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_intro.html Section_start.html Section_commands.html Section_howto.html Section_example.html Section_perf.html Section_tools.html Section_modify.html Section_python.html Section_errors.html Section_history.html [a-z]*.html
 
 txt2html *.txt
