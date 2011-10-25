fba38397dcd80c1c2891fe4919078919b5e84656
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2011-10-25 00:22:27 +0000

    SVN commit 7

--- src/run.cpp
+++ src/run.cpp
@@ -1,14 +1,15 @@
 /* ----------------------------------------------------------------------
-   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   http://lammps.sandia.gov, Sandia National Laboratories
-   Steve Plimpton, sjplimp@sandia.gov
+   DSMC - Sandia parallel DSMC code
+   www.sandia.gov/~sjplimp/dsmc.html
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
 
-   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   Copyright (2011) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
    certain rights in this software.  This software is distributed under 
    the GNU General Public License.
 
-   See the README file in the top-level LAMMPS directory.
+   See the README file in the top-level DSMC directory.
 ------------------------------------------------------------------------- */
 
 #include "dsmctype.h"
