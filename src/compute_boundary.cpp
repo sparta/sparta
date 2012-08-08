880e213975fcc113956cbeb9e1e04d23ad027d6e
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2012-08-08 21:35:56 +0000

    SVN commit 315

---
+++ src/compute_boundary.cpp
@@ -0,0 +1,255 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2012) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#include "string.h"
+#include "compute_boundary.h"
+#include "particle.h"
+#include "mixture.h"
+#include "surf.h"
+#include "update.h"
+#include "domain.h"
+#include "modify.h"
+#include "math_extra.h"
+#include "memory.h"
+#include "error.h"
+
+using namespace SPARTA_NS;
+
+enum{NUM,PRESS,XSHEAR,YSHEAR,ZSHEAR,KE};
+enum{PERIODIC,OUTFLOW,REFLECT,SURFACE};     // same as Domain
+enum{XLO,XHI,YLO,YHI,ZLO,ZHI,INTERIOR};     // same as Domain
+
+/* ---------------------------------------------------------------------- */
+
+ComputeBoundary::ComputeBoundary(SPARTA *sparta, int narg, char **arg) :
+  Compute(sparta, narg, arg)
+{
+  if (narg < 4) error->all(FLERR,"Illegal compute boundary command");
+
+  imix = particle->find_mixture(arg[2]);
+  if (imix < 0) error->all(FLERR,"Compute boundary mixture ID does not exist");
+
+  nvalue = narg - 3;
+  which = new int[nvalue];
+
+  nvalue = 0;
+  int iarg = 3;
+  while (iarg < narg) {
+    if (strcmp(arg[iarg],"n") == 0) which[nvalue++] = NUM;
+    else if (strcmp(arg[iarg],"press") == 0) which[nvalue++] = PRESS;
+    else if (strcmp(arg[iarg],"shx") == 0) which[nvalue++] = XSHEAR;
+    else if (strcmp(arg[iarg],"shy") == 0) which[nvalue++] = YSHEAR;
+    else if (strcmp(arg[iarg],"shz") == 0) which[nvalue++] = ZSHEAR;
+    else if (strcmp(arg[iarg],"ke") == 0) which[nvalue++] = KE;
+    else error->all(FLERR,"Illegal compute boundary command");
+    iarg++;
+  }
+
+  boundary_tally_flag = 1;
+  timeflag = 1;
+  array_flag = 1;
+  ngroup = particle->mixture[imix]->ngroup;
+  ntotal = ngroup*nvalue;
+  size_array_rows = 2 * domain->dimension;
+  size_array_cols = ntotal;
+
+  memory->create(array,size_array_rows,size_array_cols,"boundary:array");
+}
+
+/* ---------------------------------------------------------------------- */
+
+ComputeBoundary::~ComputeBoundary()
+{
+  delete [] which;
+  memory->destroy(array);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeBoundary::init()
+{
+  if (ngroup != particle->mixture[imix]->ngroup)
+    error->all(FLERR,
+               "Number of groups in compute boundary mixture has changed");
+
+  // set normflux based on box face area and timestep
+
+  double dt = update->dt;
+  if (domain->dimension == 2) {
+    normflux[XLO] = normflux[XHI] = domain->yprd * dt;
+    normflux[YLO] = normflux[YHI] = domain->xprd * dt;
+  } else if (domain->dimension == 3) {
+    normflux[XLO] = normflux[XHI] = domain->yprd*domain->zprd * dt;
+    normflux[YLO] = normflux[YHI] = domain->xprd*domain->xprd * dt;
+    normflux[ZLO] = normflux[ZHI] = domain->xprd*domain->yprd * dt;
+  }
+
+  // initialize tally array in case accessed without a timestep of tallying
+
+  clear();
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeBoundary::compute_array()
+{
+  invoked_array = update->ntimestep;
+
+  // normalize tallied values
+
+  int m;
+  for (int j = 0; j < ntotal; j++) {
+    m = j % nvalue;
+    if (which[m] != NUM) 
+      for (int i = 0; i < size_array_rows; i++) {
+        array[i][j] /= normflux[i];
+      }
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeBoundary::clear()
+{
+  for (int i = 0; i < size_array_rows; i++)
+    for (int j = 0; j < ntotal; j++)
+      array[i][j] = 0.0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeBoundary::boundary_tally(int iface, int istyle,
+                                     double *vold, Particle::OnePart *p)
+{
+  // skip species not in mixture group
+
+  int ispecies = p->ispecies;
+  int igroup = particle->mixture[imix]->species2group[ispecies];
+  if (igroup < 0) return;
+
+  // tally all values associated with group into array
+  // styles PERIODIC and OUTFLOW do not have post-bounce velocity
+  // nflag and tflag set if normal and tangent computation already done once
+
+  double vnorm[3],vdelta[3],vtang[3];
+  double pre,post,vsqpre,vsqpost;
+
+  double *norm = domain->norm[iface];
+  double mass = particle->species[ispecies].mass;
+  double mvv2e = update->mvv2e;
+
+  int k = igroup*nvalue;
+  int nflag = 0;
+  int tflag = 0;
+
+  if (istyle == PERIODIC) {
+    for (int m = 0; m < nvalue; m++) {
+      if (which[m] == NUM) array[iface][k++] += 1.0;
+      else k++;
+    }
+
+  } else if (istyle == OUTFLOW) {
+    for (int m = 0; m < nvalue; m++) {
+      switch (which[m]) {
+      case NUM:
+        array[iface][k++] += 1.0;
+        break;
+      case PRESS:
+        if (!nflag) {
+          nflag = 1;
+          pre = MathExtra::dot3(vold,norm);
+        }
+        array[iface][k++] -= mass * pre;
+        break;
+      case XSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::scale3(MathExtra::dot3(vold,norm),norm,vnorm);
+          MathExtra::sub3(vold,vnorm,vtang);
+        }
+        array[iface][k++] += mass * vtang[0];
+        break;
+      case YSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::scale3(MathExtra::dot3(vold,norm),norm,vnorm);
+          MathExtra::sub3(vold,vnorm,vtang);
+        }
+        array[iface][k++] += mass * vtang[1];
+        break;
+      case ZSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::scale3(MathExtra::dot3(vold,norm),norm,vnorm);
+          MathExtra::sub3(vold,vnorm,vtang);
+        }
+        array[iface][k++] += mass * vtang[2];
+        break;
+      case KE:
+        vsqpre = MathExtra::lensq3(vold);
+        array[iface][k++] += 0.5 * mvv2e * mass * vsqpre;
+        break;
+      }
+    }
+
+  } else {
+    for (int m = 0; m < nvalue; m++) {
+      switch (which[m]) {
+      case NUM:
+        array[iface][k++] += 1.0;
+        break;
+      case PRESS:
+        if (!nflag) {
+          nflag = 1;
+          pre = MathExtra::dot3(vold,norm);
+          post = MathExtra::dot3(p->v,norm);
+        }
+        array[iface][k++] += mass * (post-pre);
+        break;
+      case XSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::sub3(p->v,vold,vdelta);
+          MathExtra::scale3(MathExtra::dot3(vdelta,norm),norm,vnorm);
+          MathExtra::sub3(vdelta,vnorm,vtang);
+        }
+        array[iface][k++] -= mass * vtang[0];
+        break;
+      case YSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::sub3(p->v,vold,vdelta);
+          MathExtra::scale3(MathExtra::dot3(vdelta,norm),norm,vnorm);
+          MathExtra::sub3(vdelta,vnorm,vtang);
+        }
+        array[iface][k++] -= mass * vtang[1];
+        break;
+      case ZSHEAR:
+        if (!tflag) {
+          tflag = 1;
+          MathExtra::sub3(p->v,vold,vdelta);
+          MathExtra::scale3(MathExtra::dot3(vdelta,norm),norm,vnorm);
+          MathExtra::sub3(vdelta,vnorm,vtang);
+        }
+        array[iface][k++] -= mass * vtang[2];
+        break;
+      case KE:
+        vsqpre = MathExtra::lensq3(vold);
+        vsqpost = MathExtra::lensq3(p->v);
+        array[iface][k++] -= 0.5*mvv2e*mass * (vsqpost-vsqpre);
+        break;
+      }
+    }
+  }
+}
---
+++ src/compute_boundary.h
@@ -0,0 +1,47 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2012) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#ifdef COMPUTE_CLASS
+
+ComputeStyle(boundary,ComputeBoundary)
+
+#else
+
+#ifndef SPARTA_BOUNDARY_SURF_H
+#define SPARTA_BOUNDARY_SURF_H
+
+#include "compute.h"
+#include "particle.h"
+
+namespace SPARTA_NS {
+
+class ComputeBoundary : public Compute {
+ public:
+  ComputeBoundary(class SPARTA *, int, char **);
+  ~ComputeBoundary();
+  void init();
+  void compute_array();
+  void clear();
+  void boundary_tally(int, int, double *, Particle::OnePart *);
+
+ private:
+  int imix,nvalue,ngroup,ntotal;
+  int *which;
+  double normflux[6];
+};
+
+}
+
+#endif
+#endif
