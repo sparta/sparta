b76e967641584e2e36b8cac88cbb2ab834fb6dac
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2012-08-15 17:17:18 +0000

    SVN commit 346

--- python/install.py
+++ python/install.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-instructions = """copy LAMMPS shared library src/liblammps.so and lammps.py to system dirs
+instructions = """copy SPARTA shared library src/libsparta.so and sparta.py to system dirs
 Syntax: python install.py [libdir] [pydir]
-    libdir = target dir for src/liblammps.so, default = /usr/local/lib, or the first
+    libdir = target dir for src/libsparta.so, default = /usr/local/lib, or the first
         item in LD_LIBRARY_PATH if it doesn't exist.
-    pydir = target dir for lammps.py, default = Python site-packages, via distutils."""
+    pydir = target dir for sparta.py, default = Python site-packages, via distutils."""
 
 import sys, shutil, os
 
@@ -16,37 +16,36 @@ if len(sys.argv) > 3:
 # since if not, the install won't work
   
 libdir = "/usr/local/lib"
-libpaths = os.environ['LD_LIBRARY_PATH'].split(':')
-if not libdir in libpaths:
-    libdir = libpaths[0]
+if "LD_LIBRARY_PATH" in os.environ:
+  libpaths = os.environ['LD_LIBRARY_PATH'].split(':')
+  if not libdir in libpaths: libdir = libpaths[0]
 
 pydir = False
 try:
-    libdir = sys.argv[1]
-    pydir = sys.argv[2]
+  libdir = sys.argv[1]
+  pydir = sys.argv[2]
 except IndexError:
-    pass
+  pass
 
 # copy the C library into place
 
-shutil.copy('../src/liblammps.so', libdir)
+shutil.copy('../src/libsparta.so', libdir)
 
-# if user-specified, copy lammps.py into directory
+# if user-specified, copy sparta.py into directory
 # else invoke setup from Distutils to add to site-packages
 
 if pydir:
-    shutil.copy('../python/lammps.py', pydir)
-    sys.exit()
+  shutil.copy('../python/sparta.py', pydir)
+  sys.exit()
 
 from distutils.core import setup
 
 os.chdir('../python')
 
-setup(name = "lammps",
-    version = "15Aug12",
-    author = "Steve Plimpton",
-    author_email = "sjplimp@sandia.gov",
-    url = "http://lammps.sandia.gov",
-    description = """LAMMPS molecular dynamics library""",
-    py_modules = ["lammps"]
-)
+setup(name = "sparta",
+      version = "15Aug12",
+      author = "Steve Plimpton",
+      author_email = "sjplimp@sandia.gov",
+      url = "http://sparta.sandia.gov",
+      description = """SPARTA DSMC library""",
+      py_modules = ["sparta"])
--- python/sparta.py
+++ python/sparta.py
@@ -1,16 +1,16 @@
-/* ----------------------------------------------------------------------
-   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
-   http://sparta.sandia.gov
-   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
-   Sandia National Laboratories
-
-   Copyright (2012) Sandia Corporation.  Under the terms of Contract
-   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under 
-   the GNU General Public License.
-
-   See the README file in the top-level SPARTA directory.
-------------------------------------------------------------------------- */
+# ----------------------------------------------------------------------
+#   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+#   http://sparta.sandia.gov
+#   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+#   Sandia National Laboratories
+#
+#   Copyright (2012) Sandia Corporation.  Under the terms of Contract
+#   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+#   certain rights in this software.  This software is distributed under 
+#   the GNU General Public License.
+#
+#   See the README file in the top-level SPARTA directory.
+# -------------------------------------------------------------------------
 
 # Python wrapper on SPARTA library via ctypes
 
@@ -33,28 +33,28 @@ class sparta:
     # don't know how to pass an MPI communicator from PyPar
     # no_mpi call lets SPARTA use MPI_COMM_WORLD
     # cargs = array of C strings from args
-    
-    if args:
-      args.insert(0,"sparta.py")
-      narg = len(args)
-      cargs = (c_char_p*narg)(*args)
-      self.sparta = c_void_p()
-      self.lib.sparta_open_no_mpi(narg,cargs,byref(self.sparta))
+
+    if cmdargs:
+      cmdargs.insert(0,"sparta.py")
+      narg = len(cmdargs)
+      cargs = (c_char_p*narg)(*cmdargs)
+      self.spa = c_void_p()
+      self.lib.sparta_open_no_mpi(narg,cargs,byref(self.spa))
     else:
-      self.sparta = c_void_p()
-      self.lib.sparta_open_no_mpi(0,None,byref(self.sparta))
-      # could use just this if SPARTA lib interface supported it
-      # self.sparta = self.lib.sparta_open_no_mpi(0,None)
+      self.spa = c_void_p()
+      self.lib.sparta_open_no_mpi(0,None,byref(self.spa))
+      # could use just this if LAMMPS lib interface supported it
+      # self.lmp = self.lib.lammps_open_no_mpi(0,None)
 
   def __del__(self):
-    if self.sparta: self.lib.sparta_close(self.sparta)
+    if self.spa: self.lib.sparta_close(self.spa)
 
   def close(self):
-    self.lib.sparta_close(self.sparta)
-    self.sparta = None
+    self.lib.sparta_close(self.spa)
+    self.spa = None
 
   def file(self,file):
-    self.lib.sparta_file(self.sparta,file)
+    self.lib.sparta_file(self.spa,file)
 
   def command(self,cmd):
-    self.lib.sparta_command(self.sparta,cmd)
+    self.lib.sparta_command(self.spa,cmd)
