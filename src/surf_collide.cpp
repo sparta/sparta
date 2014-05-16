f3d64f86277d972acba2468360ccf4ec681e429d
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2014-05-16 16:24:12 +0000

    SVN commit 730

--- src/surf_collide.cpp
+++ src/surf_collide.cpp
@@ -12,18 +12,29 @@
    See the README file in the top-level SPARTA directory.
 ------------------------------------------------------------------------- */
 
+#include "ctype.h"
 #include "string.h"
 #include "surf_collide.h"
+#include "error.h"
 
 using namespace SPARTA_NS;
 
 /* ---------------------------------------------------------------------- */
 
-SurfCollide::SurfCollide(SPARTA *sparta, int narg, char **arg) : Pointers(sparta)
+SurfCollide::SurfCollide(SPARTA *sparta, int narg, char **arg) : 
+  Pointers(sparta)
 {
+  // ID and style
+  // ID must be all alphanumeric chars or underscores
+
   int n = strlen(arg[0]) + 1;
   id = new char[n];
   strcpy(id,arg[0]);
+
+  for (int i = 0; i < n-1; i++)
+    if (!isalnum(id[i]) && id[i] != '_')
+      error->all(FLERR,"Surf_collide ID must be alphanumeric or "
+                 "underscore characters");
 
   n = strlen(arg[0]) + 1;
   style = new char[n];
