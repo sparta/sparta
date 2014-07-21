54bc0b4a937213fb41b8a091d1bfcbfa779dfb8a
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2014-07-21 16:08:23 +0000

    SVN commit 1029

--- doc/PDFgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-# generate a PDF version of Manual
-
-txt2html -b *.txt
-
-htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_intro.html Section_start.html Section_commands.html Section_howto.html Section_example.html Section_perf.html Section_tools.html Section_modify.html Section_python.html Section_errors.html Section_history.html [a-z]*.html
-
-txt2html *.txt
