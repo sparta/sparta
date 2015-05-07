9fc607eef16d19fd1c2664219f7272d70b68670d
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2015-05-07 20:47:51 +0000

    SVN commit 1449

---
+++ src/fix_emit.cpp
@@ -0,0 +1,401 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#include "stdlib.h"
+#include "string.h"
+#include "fix_emit.h"
+#include "update.h"
+#include "domain.h"
+#include "region.h"
+#include "grid.h"
+#include "comm.h"
+#include "random_mars.h"
+#include "random_park.h"
+#include "math_const.h"
+#include "memory.h"
+#include "error.h"
+
+using namespace SPARTA_NS;
+using namespace MathConst;
+
+enum{UNKNOWN,OUTSIDE,INSIDE,OVERLAP};           // same as Grid
+
+#define DELTAGRID 1024
+
+/* ---------------------------------------------------------------------- */
+
+FixEmit::FixEmit(SPARTA *sparta, int narg, char **arg) :
+  Fix(sparta, narg, arg)
+{
+  vector_flag = 1;
+  size_vector = 2;
+  global_freq = 1;
+  gridmigrate = 1;
+
+  // RNG
+
+  int me = comm->me;
+  random = new RanPark(update->ranmaster->uniform());
+  double seed = update->ranmaster->uniform();
+  random->reset(seed,me,100);
+
+  // local storage of emit data structures
+
+  c2list = NULL;
+  nglocal = nglocalmax = 0;
+  clist = clistnum = clistfirst = NULL;
+  nlist = nlistmax = 0;
+
+  // counters common to all emit styles for output from fix
+
+  nsingle = ntotal = 0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixEmit::~FixEmit()
+{
+  delete random;
+
+  memory->destroy(c2list);
+  memory->destroy(clist);
+  memory->destroy(clistnum);
+  memory->destroy(clistfirst);
+}
+
+/* ---------------------------------------------------------------------- */
+
+int FixEmit::setmask()
+{
+  int mask = 0;
+  mask |= START_OF_STEP;
+  return mask;
+}
+
+/* ----------------------------------------------------------------------
+   initialize per grid cell task data for grid cells I own
+   do this from scratch every run in case grid or emit properties change
+   callback to child emit style onecell() for each cell
+   called from init() of child emit styles
+------------------------------------------------------------------------- */
+
+void FixEmit::init()
+{
+  particle->exist = 1;
+  ntotal = 0;
+
+  int dimension = domain->dimension;
+  Grid::ChildCell *cells = grid->cells;
+  Grid::ChildInfo *cinfo = grid->cinfo;
+  nglocal = grid->nlocal;
+
+  if (nglocal > nglocalmax) {
+    memory->destroy(c2list);
+    nglocalmax = nglocal;
+    memory->create(c2list,nglocalmax,"emit:c2list");
+  }
+
+  // upsplit, split, sub cells store c2list flag
+  // upsplit, split cells can store clist data, but only if have tasks
+  // no tasks for a cell inside surface
+  // no tasks if cell is entirely outside region bounding box
+
+  int flag,ntaskcell,ntaskfirst;
+
+  nlist = 0;
+  ntaskfirst = 0;
+
+  for (int icell = 0; icell < nglocal; icell++) {
+    c2list[icell] = -1;
+    if (cells[icell].nsplit <= 0) continue;
+    if (cinfo[icell].type == INSIDE) continue;
+    if (region && region->bboxflag) {
+      flag = 1;
+      if (cells[icell].hi[0] > region->extent_xlo &&
+          cells[icell].lo[0] < region->extent_xhi) flag = 0;
+      if (cells[icell].hi[1] > region->extent_ylo &&
+          cells[icell].lo[1] < region->extent_yhi) flag = 0;
+      if (dimension == 3) {
+        if (cells[icell].hi[2] > region->extent_zlo &&
+            cells[icell].lo[2] < region->extent_zhi) flag = 0;
+      }
+      if (flag) continue;
+    }
+
+    ntaskcell = create_task(icell);
+    if (ntaskcell) {
+      if (nlist == nlistmax) {
+	nlistmax += DELTAGRID;
+	memory->grow(clist,nlistmax,"emit:clist");
+	memory->grow(clistnum,nlistmax,"emit:clistnum");
+	memory->grow(clistfirst,nlistmax,"emit:clistfirst");
+      }
+      c2list[icell] = nlist;
+      clist[nlist] = icell;
+      clistnum[nlist] = ntaskcell;
+      clistfirst[nlist] = ntaskfirst;
+      ntaskfirst += ntaskcell;
+      nlist++;
+    }
+  }
+}
+
+/* ----------------------------------------------------------------------
+   perform all particle emit tasks for cells I own
+------------------------------------------------------------------------- */
+
+void FixEmit::start_of_step()
+{
+  if (update->ntimestep % nevery) return;
+
+  nsingle = 0;
+  perform_task();
+  ntotal += nsingle;
+}
+
+/* ----------------------------------------------------------------------
+   pack task count and tasks for grid cell icell into buf
+   return byte count of amount packed
+   if not memflag, only return count, do not fill buf
+------------------------------------------------------------------------- */
+
+int FixEmit::pack_grid_one(int icell, char *buf, int memflag)
+{
+  char *ptr = buf;
+
+  // ntaskcell = task count for icell
+
+  int ilist = c2list[icell];
+
+  int ntaskcell = 0;
+  if (ilist >= 0) ntaskcell = clistnum[ilist];
+
+  if (memflag) memcpy(ptr,&ntaskcell,sizeof(int));
+  ptr += sizeof(int);
+  ptr = ROUNDUP(ptr);
+
+  if (!ntaskcell) return ptr-buf;
+
+  // pack each insert task via child class pack_task()
+
+  int itask = clistfirst[ilist];
+  for (int i = 0; i < ntaskcell; i++) {
+    ptr += pack_task(itask,ptr,memflag);
+    ptr = ROUNDUP(ptr);
+    itask++;
+  }
+
+  return ptr-buf;
+}
+
+/* ----------------------------------------------------------------------
+   unpack task flag and tasks for grid cell icell from buf
+   also unpack cellface data for flagged faces
+   return byte count of amount unpacked
+------------------------------------------------------------------------- */
+
+int FixEmit::unpack_grid_one(int icell, char *buf)
+{
+  char *ptr = buf;
+
+  if (nglocal == nglocalmax) grow_percell(1);
+
+  int ntaskcell;
+  memcpy(&ntaskcell,ptr,sizeof(int));
+  ptr += sizeof(int);
+  ptr = ROUNDUP(ptr);
+
+  if (ntaskcell) c2list[icell] = nlist;
+  else c2list[icell] = -1;
+  nglocal++;
+
+  // if new cell is split cell, set c2list for subcells to -1
+
+  int nsplit = grid->cells[icell].nsplit;
+  if (nsplit > 1) {
+    if (nglocal+nsplit > nglocalmax) grow_percell(nsplit);
+    for (int i = 0; i < nsplit; i++) c2list[nglocal++] = -1;
+  }
+
+  if (!ntaskcell) return ptr-buf;
+
+  // add list entry to all clist vectors
+
+  if (nlist == nlistmax) grow_list();
+  clist[nlist] = icell;
+  clistnum[nlist] = ntaskcell;
+  if (nlist) clistfirst[nlist] = clistfirst[nlist-1] + clistnum[nlist-1];
+  else clistfirst[nlist] = 0;
+  nlist++;
+
+  // unpack each insert task via unpack_task() provided by child class
+
+  for (int i = 0; i < ntaskcell; i++) {
+    ptr += unpack_task(ptr,icell);
+    ptr = ROUNDUP(ptr);
+  }
+
+  return ptr-buf;
+}
+
+/* ----------------------------------------------------------------------
+   compress per-cell data due to cells migrating to new procs
+   criteria for keeping/discarding a cell is same as in Grid::compress()
+   this keeps final ordering of per-cell data consistent with Grid class
+------------------------------------------------------------------------- */
+
+void FixEmit::compress_grid()
+{
+  int me = comm->me;
+  Grid::ChildCell *cells = grid->cells;
+
+  // keep an unsplit or split cell if staying on this proc
+  // keep a sub cell if its split cell is staying on this proc
+  // kept upsplit, split, sub cells store c2list flag
+  // kept upsplit, split cells store clist data only if have tasks
+
+  int ncurrent = nglocal;
+  nglocal = 0;
+
+  int oldlist,ntaskcell,oldntaskfirst;
+  int ntaskfirst = 0;
+  nlist = ntask = 0;
+
+  for (int icell = 0; icell < ncurrent; icell++) {
+    if (cells[icell].nsplit >= 1) {
+      if (cells[icell].proc != me) continue;
+      if (c2list[icell] < 0) {
+	c2list[nglocal++] = -1;
+	continue;
+      }
+    } else {
+      int isplit = cells[icell].isplit;
+      if (cells[grid->sinfo[isplit].icell].proc != me) continue;
+      c2list[nglocal++] = -1;
+      continue;
+    }
+
+    oldlist = c2list[icell];
+    ntaskcell = clistnum[oldlist];
+    oldntaskfirst = clistfirst[oldlist];
+
+    c2list[nglocal] = nlist;
+    clist[nlist] = nglocal++;
+    clistnum[nlist] = ntaskcell;
+    clistfirst[nlist] = ntaskfirst;
+    copy_task(icell,ntaskcell,ntaskfirst,oldntaskfirst);
+    ntaskfirst += ntaskcell;
+    nlist++;
+  }
+}
+
+/* ----------------------------------------------------------------------
+   insure c2list allocated long enough for N new cells
+------------------------------------------------------------------------- */
+
+void FixEmit::grow_percell(int n)
+{
+  while (nglocal+n > nglocalmax) nglocalmax += DELTAGRID;
+  memory->grow(c2list,nglocalmax,"emit:c2list");
+}
+
+/* ----------------------------------------------------------------------
+   grow all Nlist vectors
+------------------------------------------------------------------------- */
+
+void FixEmit::grow_list()
+{
+  nlistmax += DELTAGRID;
+  memory->grow(clist,nlistmax,"emit:clist");
+  memory->grow(clistnum,nlistmax,"emit:clistnum");
+  memory->grow(clistfirst,nlistmax,"emit:clistfirst");
+}
+
+/* ----------------------------------------------------------------------
+   calculate flux of particles of a species with vscale/fraction
+     entering a grid cell
+   indot = vstream dotted into face normal, assumed to be >= 0.0
+   scosine = s cos(theta) in Bird notation where vscale = 1/beta
+   see Bird 1994, eq 4.22
+------------------------------------------------------------------------- */
+
+double FixEmit::mol_inflow(double indot, double vscale, double fraction)
+{
+  double scosine = indot / vscale;
+  double inward_number_flux = vscale*fraction *
+    (exp(-scosine*scosine) + sqrt(MY_PI)*scosine*(1.0 + erf(scosine))) / 
+    (2*sqrt(MY_PI));
+  return inward_number_flux;
+}
+
+/* ----------------------------------------------------------------------
+   process optional keywords common to all emit styles
+   pass unrecognized keyword back to child option() method
+   called from constructor of child emit styles
+------------------------------------------------------------------------- */
+
+void FixEmit::options(int narg, char **arg)
+{
+  nevery = 1;
+  perspecies = 1;
+  region = NULL;
+
+  int iarg = 0;
+  while (iarg < narg) {
+    if (strcmp(arg[iarg],"nevery") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix emit command");
+      nevery = atoi(arg[iarg+1]);
+      if (nevery <= 0) error->all(FLERR,"Illegal fix emit command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"perspecies") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix emit command");
+      if (strcmp(arg[iarg+1],"yes") == 0) perspecies = 1;
+      else if (strcmp(arg[iarg+1],"no") == 0) perspecies = 0;
+      else error->all(FLERR,"Illegal fix emit command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"region") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix emit command");
+      int iregion = domain->find_region(arg[iarg+1]);
+      if (iregion < 0) 
+        error->all(FLERR,"Fix emit region does not exist");
+      region = domain->regions[iregion];
+      iarg += 2;
+
+    } else iarg += option(narg-iarg,&arg[iarg]);
+  }
+}
+
+/* ----------------------------------------------------------------------
+   process unknown keyword
+------------------------------------------------------------------------- */
+
+int FixEmit::option(int narg, char **arg)
+{
+  error->all(FLERR,"Illegal fix emit command");
+  return 0;
+}
+
+/* ----------------------------------------------------------------------
+   return one-step or total count of particle insertions
+------------------------------------------------------------------------- */
+
+double FixEmit::compute_vector(int i)
+{
+  double one,all;
+  
+  if (i == 0) one = nsingle;
+  else one = ntotal;
+  MPI_Allreduce(&one,&all,1,MPI_DOUBLE,MPI_SUM,world);
+  return all;
+}
---
+++ src/fix_emit.h
@@ -0,0 +1,75 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#ifndef SPARTA_FIX_EMIT_H
+#define SPARTA_FIX_EMIT_H
+
+#include "fix.h"
+
+namespace SPARTA_NS {
+
+class FixEmit : public Fix {
+ public:
+  FixEmit(class SPARTA *, int, char **);
+  virtual ~FixEmit();
+  int setmask();
+  virtual void init();
+  void start_of_step();
+  double compute_vector(int);
+
+  int pack_grid_one(int, char *, int);
+  int unpack_grid_one(int, char *);
+  void compress_grid();
+  virtual void post_compress_grid() {}
+
+ protected:
+  int perspecies;
+  class Region *region;
+  class RanPark *random;
+  int nsingle,ntotal;
+
+  int nglocal;         // copy of cell->nlocal
+  int nglocalmax;      // max size of c2list
+  int *c2list;         // index into clist for each owned cell
+                       // -1 if no tasks for the cell
+                       // NOTE: only for unsplit and split cells, not sub cells?
+
+  int nlist;           // # of owned cells with insert tasks
+  int nlistmax;        // max size of clist,clistnum,clistfirst
+  int *clist;          // local indices of cells with insert tasks
+  int *clistnum;       // # of insert tasks in each cell with tasks
+  int *clistfirst;     // first task ID for each cell with tasks
+
+  int ntask;           // # of insert tasks in underlying child class
+
+  virtual int create_task(int) = 0;
+  virtual void perform_task() = 0;
+  virtual int pack_task(int, char *, int) = 0;
+  virtual int unpack_task(char *, int) = 0;
+  virtual void copy_task(int, int, int, int) = 0;
+
+  void grow_percell(int);
+  void grow_list();
+  double mol_inflow(double, double, double);
+  void options(int, char **);
+  virtual int option(int, char **);
+};
+
+}
+
+#endif
+
+/* ERROR/WARNING messages:
+
+*/
