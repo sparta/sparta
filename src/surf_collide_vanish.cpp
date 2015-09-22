5ee7361af439739891244b8a9bf5c655839ca844
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2015-09-22 23:30:26 +0000

    SVN commit 1689

---
+++ src/surf_collide_vanish.cpp
@@ -0,0 +1,43 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "surf_collide_vanish.h"
+#include "error.h"
+
+using namespace SPARTA_NS;
+
+/* ---------------------------------------------------------------------- */
+
+SurfCollideVanish::SurfCollideVanish(SPARTA *sparta, int narg, char **arg) :
+  SurfCollide(sparta, narg, arg)
+{
+  if (narg != 2) error->all(FLERR,"Illegal surf_collide vanish command");
+}
+
+/* ----------------------------------------------------------------------
+   particle collision with surface with optional chemistry
+   ip = particle with current x = collision pt, current v = incident v
+   norm = surface normal unit vector
+   simply return ip = NULL to delete particle
+------------------------------------------------------------------------- */
+
+Particle::OnePart *SurfCollideVanish::
+collide(Particle::OnePart *&ip, double *norm, int isr)
+{
+  nsingle++;
+
+  ip = NULL;
+  return NULL;
+}
