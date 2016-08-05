bd097be0990b4b90a9fa6579030bde7f14eaefd6
sjplimp <sjplimp@59f801cd-88df-4bd4-8d19-159065443089>
2016-08-05 18:05:38 +0000

    SVN commit 2091

--- src/STUBS/mpi.c
+++ src/STUBS/mpi.c
@@ -1,15 +1,16 @@
-/* -----------------------------------------------------------------------
-   LAMMPS 2003 (July 31) - Molecular Dynamics Simulator
-   Sandia National Laboratories, www.cs.sandia.gov/~sjplimp/lammps.html
-   Steve Plimpton, sjplimp@sandia.gov
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
 
-   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under
+   certain rights in this software.  This software is distributed under 
    the GNU General Public License.
 
-   See the README file in the top-level LAMMPS directory.
------------------------------------------------------------------------- */
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
 
 /* Single-processor "stub" versions of MPI routines */
 /* -I. in Makefile insures dummy mpi.h in this dir is included */
