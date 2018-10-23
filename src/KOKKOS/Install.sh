1bcfea83c46a34a67d75340a1b40ea178cd8e5d6
Stan Moore <stamoor@sandia.gov>
2018-10-23 13:55:48 -0600

    Code from Alan Stagg: fix bug in compute_sonine_grid and add Kokkos version

--- src/KOKKOS/Install.sh
+++ src/KOKKOS/Install.sh
@@ -61,6 +61,8 @@ action compute_lambda_grid_kokkos.cpp
 action compute_lambda_grid_kokkos.h
 action compute_pflux_grid_kokkos.cpp
 action compute_pflux_grid_kokkos.h
+action compute_sonine_grid_kokkos.cpp
+action compute_sonine_grid_kokkos.h
 action compute_temp_kokkos.cpp
 action compute_temp_kokkos.h
 action compute_thermal_grid_kokkos.cpp
