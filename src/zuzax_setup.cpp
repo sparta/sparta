deee5ca66cc8146810fd963ad87e9e226e274463
Harry Moffat <hkmoffa@sandia.gov>
2019-05-12 15:42:52 -0600

    created a zuzax_setup class and added into the global pointers

---
+++ src/zuzax_setup.cpp
@@ -0,0 +1,44 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "ctype.h"
+#include "stdlib.h"
+#include "string.h"
+#include "zuzax_setup.h"
+
+namespace SPARTA_NS {
+
+
+//=================================================================================================
+ZuzaxSetup::ZuzaxSetup(SPARTA *sparta) : 
+    Pointers(sparta) 
+{
+}
+//=================================================================================================
+ZuzaxSetup::~ZuzaxSetup()
+{
+}
+//=================================================================================================
+void ZuzaxSetup::init()
+{
+}
+//=================================================================================================
+void ZuzaxSetup::initGasSetup(int nargs, char** args)
+{
+
+}
+//=================================================================================================
+}
+
---
+++ src/zuzax_setup.h
@@ -0,0 +1,61 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@sandia.gov, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#ifndef SPARTA_ZUZAX_SETUP_H
+#define SPARTA_ZUZAX_SETUP_H
+
+#include "cstdio"
+#include "pointers.h"
+
+namespace SPARTA_NS {
+
+class ZuzaxSetup : protected Pointers {
+public:
+
+    //! Constructor 
+    /*!
+     *  @param(in)           sparta              Pointer to the SPARTA class containing a list
+     *                                           of common pointers
+     */
+    ZuzaxSetup(class SPARTA * sparta);
+
+    //! Virtual destructor
+    virtual ~ZuzaxSetup();
+
+    //! initializer
+    void init();
+
+    //! Read the Zuzax gas xml file, setting up NASA polynomials 
+    /*!
+     *  Nasa polynomials allow for a consistent energy level between gas, surf, and solid species.
+     *
+     *  This will contain one argument, the name of the gas ThermoPhase file.
+     *  There must be zuzax species for every sparta species. Extra zuzax species are treated as
+     *  having zero concentrations.
+     *
+     *  @param[in]           nargs               number of args
+     *  @param[in]           args                vector of args
+     */
+    void initGasSetup(int nargs, char** args);
+
+
+ protected:
+
+
+};
+
+}
+
+#endif
+
