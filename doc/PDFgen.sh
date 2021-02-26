75fec7ab3a3dfb6c4937d290f67057e06b30f77c
Steve Plimpton <sjplimp@sandia.gov>
2021-02-26 16:46:51 -0700

    remove PDFgen.sh file, add to .gitignore instead

--- doc/PDFgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-# generate a PDF version of Manual
-
-/home/sjplimp/tools/txt2html/txt2html -b *.txt
-
-htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_intro.html Section_start.html Section_commands.html Section_howto.html Section_example.html Section_perf.html Section_tools.html Section_modify.html Section_errors.html [a-z]*.html
-
-/home/sjplimp/tools/txt2html/txt2html *.txt
