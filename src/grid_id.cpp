29bad255cd5b0fa009024ce3e114254c6d960d8d
Plimpton <sjplimp@s957182.srn.sandia.gov>
2021-03-30 16:53:17 -0600

    debug

--- src/grid_id.cpp
+++ src/grid_id.cpp
@@ -190,9 +190,12 @@ void Grid::id_find_child_uniform_level(int level, int lohi,
   //  decrement or increment of index
   // but 0 <= index <= N-1 is still required
 
-  if (surfID == 362) printf("Uniform for surf 362: xyz grid %d %d %d "
-			    "lohi %d x %g %g clo %g %g chi %g %g\n",
-			    xgrid,ygrid,zgrid,lohi,x[0],x[1],lo[0],lo[1],hi[0],hi[1]);
+  if (surfID == 362) {
+    printf("Uniform for surf 362: xyz grid %d %d %d "
+	   "lohi %d x %g %g clo %g %g chi %g %g\n",
+	   xgrid,ygrid,zgrid,lohi,x[0],x[1],lo[0],lo[1],hi[0],hi[1]);
+    printf("Compare x=lo %d x<lo %d x>lo %d\n",x[0]==lo[0],x[0]<lo[0],x[0]>lo[0]);
+  }
   
   if (lohi == 0) {
     if (x[0] == lo[0] && x[0] != boxlo[0]) xgrid--;
