9fd6bf30495bccf82f819686c6dd1c99dbcda527
Plimpton <sjplimp@s957182.srn.sandia.gov>
2021-04-12 17:15:28 -0600

    debug

--- src/cut3d.cpp
+++ src/cut3d.cpp
@@ -850,17 +850,12 @@ void Cut3d::clip_tris()
             if (idir == 0) {
 	      between(p1,p2,dim,value,edge->p1);
 	      if (id == VERBOSE_ID && i == 0)
-		printf("CLIPOUT iedge %d iface %d ptflag %d %20.16g %20.16g %20.16g "
+		printf("CLIPOUT iedge %d iface %d p2flag %d %20.16g %20.16g %20.16g "
 		       "hi %20.16g %20.16g\n", 
 		       i,iface,
 		       edge->p1[0],edge->p1[1],edge->p1[2],hi[1],hi[2]);
             } else {
 	      between(p1,p2,dim,value,edge->p2);
-	      if (id == VERBOSE_ID && i == 0)
-		printf("CLIPOUT iedge %d iface %d ptflag %d %20.16g %20.16g %20.16g "
-		       "hi %20.16g %20.16g\n", 
-		       i,iface,
-		       edge->p2[0],edge->p2[1],edge->p2[2],hi[1],hi[2]);
             }
 	    edge->clipped = 1;
           }
@@ -875,11 +870,6 @@ void Cut3d::clip_tris()
 		       edge->p2[0],edge->p2[1],edge->p2[2],hi[1],hi[2]);
 	    } else {
 	      between(p1,p2,dim,value,edge->p1);
-	      if (id == VERBOSE_ID && i == 0)
-		printf("CLIPIN iedge %d iface %d p2flag %d %20.16g %20.16g %20.16g "
-		       "hi %20.16g %20.16g\n",
-		       i,iface,
-		       edge->p1[0],edge->p1[1],edge->p1[2],hi[1],hi[2]);
 	    }
             edge->clipped = 1;
           }
