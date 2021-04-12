2b7451929f4945c199f7b8ac94629ba2e1ddd78f
Plimpton <sjplimp@s957182.srn.sandia.gov>
2021-04-12 17:24:15 -0600

    debug

--- src/cut3d.cpp
+++ src/cut3d.cpp
@@ -847,35 +847,29 @@ void Cut3d::clip_tris()
         if (p1flag == OUTSIDE) {
           if (p2flag == OUTSIDE || p2flag == OVERLAP) edge_remove(edge,idir);
           else {
-            if (idir == 0) {
-	      between(p1,p2,dim,value,edge->p1);
-	      if (id == VERBOSE_ID && i == 0)
-		printf("CLIPOUT iedge %d iface %d p2flag %d %20.16g %20.16g %20.16g "
-		       "hi %20.16g %20.16g\n", 
-		       i,iface,p2flag,
-		       edge->p1[0],edge->p1[1],edge->p1[2],hi[1],hi[2]);
-            } else {
-	      between(p1,p2,dim,value,edge->p2);
-            }
+            if (idir == 0) between(p1,p2,dim,value,edge->p1);
+	    else between(p1,p2,dim,value,edge->p2);
 	    edge->clipped = 1;
           }
         } else if (p1flag == INSIDE) {
           if (p2flag == OUTSIDE) {
             if (idir == 0) {
 	      between(p1,p2,dim,value,edge->p2);
-	      if (id == VERBOSE_ID && i == 0)
-		printf("CLIPIN iedge %d iface %d p2flag %d %20.16g %20.16g %20.16g "
-		       "hi %20.16g %20.16g\n",
-		       i,iface,p2flag,
-		       edge->p2[0],edge->p2[1],edge->p2[2],hi[1],hi[2]);
-	    } else {
-	      between(p1,p2,dim,value,edge->p1);
+	      else between(p1,p2,dim,value,edge->p1);
 	    }
             edge->clipped = 1;
           }
         } else {
           if (p2flag == OUTSIDE) edge_remove(edge,idir);
         }
+	
+	if (id == VERBOSE_ID && i == 0)
+	  printf("CLIP iedge %d iface %d p12flags %d %d pt1 %20.16g %20.16g %20.16g "
+		 "pt2 %20.16g %20.16g %20.16g hi %20.16g %20.16g\n", 
+		 i,iface,p1flag,p2flag,
+		 edge->p1[0],edge->p1[1],edge->p1[2],
+		 edge->p2[0],edge->p2[1],edge->p2[2],
+		 hi[1],hi[2]);
 
         iedge = edge->next[idir];
         idir = edge->dirnext[idir];
