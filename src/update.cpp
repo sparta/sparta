4ea2fd63541edb8fcde444951efe575cb19a4f46
Stan Gerald Moore <stamoor@sandia.gov>
2023-04-07 17:25:25 -0600

    Fix parsing

--- src/update.cpp
+++ src/update.cpp
@@ -1617,8 +1617,11 @@ void Update::global(int narg, char **arg)
       if (fnum <= 0.0) error->all(FLERR,"Illegal global command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"optmove") == 0) {
-      optmove_flag = 1;
-      iarg += 1;
+      if (iarg+2 > narg) error->all(FLERR,"Illegal global command");
+      if (strcmp(arg[iarg+1],"yes") == 0) optmove_flag = 1;
+      else if (strcmp(arg[iarg+1],"no") == 0) optmove_flag = 0;
+      else error->all(FLERR,"Illegal global command");
+      iarg += 2;
     } else if (strcmp(arg[iarg],"nrho") == 0) {
       if (iarg+2 > narg) error->all(FLERR,"Illegal global command");
       nrho = input->numeric(FLERR,arg[iarg+1]);
