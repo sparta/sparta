b3065236bfdf50cda74db0cdd1246285081602f8
aborner1 <42391399+aborner1@users.noreply.github.com>
2023-04-25 16:52:09 -0700

    Update react_tce.h

--- src/react_tce.h
+++ src/react_tce.h
@@ -32,6 +32,19 @@ class ReactTCE : public ReactBird {
   void init();
   int attempt(Particle::OnePart *, Particle::OnePart *,
               double, double, double, double &, int &);
+
+  double newtonTvib(int nmode, double Evib,
+                      double VibTemp[],
+                      double Tvib0,
+                      double tol,
+                      int nmax);
+
+  double bird_dEvib(int nmode, double Tvib,
+                  double VibTemp[]);
+
+  double bird_Evib(int nmode, double Tvib,
+                 double VibTemp[],
+                 double Evib);
 };
 
 }
