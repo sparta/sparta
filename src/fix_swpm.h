8e6f936547aac946fe1006c2fc8c3d2a102e9bf3
Andrew Young Kyun Hong <ayhong@sandia.gov>
2024-04-02 11:48:02 -0600

    Added requirement for cpp11

---
+++ src/fix_swpm.h
@@ -0,0 +1,46 @@
+/* ----------------------------------------------------------------------
+   SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
+   http://sparta.sandia.gov
+   Steve Plimpton, sjplimp@gmail.com, Michael Gallis, magalli@sandia.gov
+   Sandia National Laboratories
+
+   Copyright (2014) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level SPARTA directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(swpm,FixSWPM)
+
+#else
+
+#ifndef SPARTA_FIX_SWPM_H
+#define SPARTA_FIX_SWPM_H
+
+#include "stdio.h"
+#include "fix.h"
+#include "particle.h"
+
+namespace SPARTA_NS {
+
+class FixSWPM : public Fix {
+ public:
+  FixSWPM(class SPARTA *, int, char **);
+  virtual ~FixSWPM();
+  int setmask();
+  void init();
+  virtual void update_custom(int, double, double, double, double *);
+
+ protected:
+  int index_swpm;
+  double sweight_new;
+};
+
+}
+
+#endif
+#endif
