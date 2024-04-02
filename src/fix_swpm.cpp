264932a4a3ca10aee64b4b3042a4103728abba69
Andrew Young Kyun Hong <ayhong@sandia.gov>
2024-04-02 01:01:11 -0600

    renamed inputs to be more coherent checked that swpm fix working

--- src/fix_swpm.cpp
+++ src/fix_swpm.cpp
@@ -40,8 +40,6 @@ FixSWPM::FixSWPM(SPARTA *sparta, int narg, char **arg) :
 
   index_swpm = particle->add_custom((char *) "sweight",DOUBLE,0);
 
-  sweight_new = atof(arg[1]);
-  printf("%4.3e\n", sweight_new);
   error->one(FLERR,"ck");
 }
 
@@ -78,12 +76,12 @@ void FixSWPM::update_custom(int index, double,
                                 double, double,
                                 double*)
 {
-  double *swpmweight = particle->edvec[particle->ewhich[index_swpm]];
+  double *sweights = particle->edvec[particle->ewhich[index_swpm]];
 
   // conditional avoids weight reset during surface collisions
   // only new particles (zero weight) have their weight set as fnum
 
-  if(swpmweight[index] == 0.0) swpmweight[index] = sweight_new;
+  if(sweights[index] == 0.0) sweights[index] = update->fnum;
 }
 
 
