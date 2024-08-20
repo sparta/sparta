7b7a2d862a30629b216b07bda71b73fac5027481
Zakari Eckert <zeckert@sandia.gov>
2024-08-20 11:34:01 -0400

    fix: Mark surf custom data for respreading after reread
    The bug reported in #496 was caused by the surface custom data spread
    being skipped after the first time a surface is read.
    
    This fixes the test added in the previous commit, however, I have not
    yet tested if this also fixes the emit/surf with custom values.

--- src/read_surf.cpp
+++ src/read_surf.cpp
@@ -157,6 +157,7 @@ void ReadSurf::command(int narg, char **arg)
 	if (flag) error->all(FLERR,"Read_surf custom attributes do not match "
 	 		     "already existing custom data");
 	index_custom[ic] = index;
+       surf->estatus[index] = 0;
       } else {
 	index_custom[ic] =
 	  surf->add_custom(name_custom[ic],type_custom[ic],size_custom[ic]);
