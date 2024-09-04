4bb99c6893d58bf6f7c7c152b6e43e043500c7f1
Steve Plimpton <sjplimp@gmail.com>
2024-09-04 15:31:22 -0600

    change links in src files

--- src/grid_surf.cpp
+++ src/grid_surf.cpp
@@ -1,6 +1,6 @@
  /* ----------------------------------------------------------------------
    SPARTA - Stochastic PArallel Rarefied-gas Time-accurate Analyzer
-   http://sparta.sandia.gov
+   http://sparta.github.io
    Steve Plimpton, sjplimp@gmail.com, Michael Gallis, magalli@sandia.gov
    Sandia National Laboratories
 
