bffc084c1c0e2b655ef9eaf7c12eabccfbf03b15
Stan Moore <stamoor@sandia.gov>
2025-09-08 12:29:36 -0600

    Make function inline to work around link error with some compilers

--- src/math_extra.h
+++ src/math_extra.h
@@ -75,7 +75,10 @@ namespace MathExtra {
   inline void vecmat(const double *v, const double m[3][3], double *ans);
   inline void scalar_times3(const double f, double m[3][3]);
 
-  int mldivide3(const double mat[3][3], const double *vec, double *ans);
+  // this function is inline to work around a link error with some compilers
+
+  inline int mldivide3(const double mat[3][3], const double *vec, double *ans);
+
   int mldivide4(const double mat[4][4], const double *vec, double *ans);
 
   // quaternion operations
